1242cf630b89c32f4f2b6c3ee4d10124ecfcb3bf
ramkishore saravanan <ramkishore07s@gmail.com>
2018-04-15 12:41:30 +0530

    refactoring

--- js/load.js
+++ js/load.js
@@ -1,11 +1,11 @@
 tunnel = createBuffers(model_tunnel2);
 obstacle = createBuffers(model_square);
 //null
-img_1 = "2.jpg";
+img_1 = "images/2.jpg";
 
 texture_0 = loadTexture(GL, img_1);
 
-img_2 = "3.jpg";
+img_2 = "images/3.jpg";
 
 texture_1 = loadTexture(GL, img_2);
 
